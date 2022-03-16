5efeef55b2d7a96dfcf76c5e26c47758242553e3
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-03-16 17:35:53 +0100

    Express.js examples

---
+++ Example06-NodeServerExpressPractice/routes/user.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../utils/path');
+
+const router = express.Router();
+
+router.get('/user', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'user.html'));
+});
+
+module.exports = router;
+
+
