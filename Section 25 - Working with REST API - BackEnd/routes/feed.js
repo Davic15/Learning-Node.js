70975e04e221446bec40f174f027f447ff7ffe96
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-04-04 23:39:07 +0200

    Section 25: Working with REST API - Backend and Frontend

--- Section 25 - Working with REST API - BackEnd/routes/feed.js
+++ Section 25 - Working with REST API - BackEnd/routes/feed.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+//* To check the request body (req)
+const { body } = require('express-validator/check')
+
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
@@ -8,6 +11,22 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 // POST -> /feed/post
-router.post('/post', feedController.createPost);
+router.post('/post', 
+    [
+        body('title').trim().isLength({ min: 5}),
+        body('content').trim().isLength({ min: 5 })
+    ],
+    feedController.createPost
+);
+
+router.get('/post/:postId', feedController.getPost);
+
+//* Adding a post
+//* It has a request body.
+router.put('/post/:postId', 
+    [
+        body('title').trim().isLength({ min: 5}),
+        body('content').trim().isLength({ min: 5 })
+    ], feedController.updatePost);
 
 module.exports = router;
